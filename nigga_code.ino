06287d5f4c0e3bd4070e83e419d526b833b24e60
nnachonesa <imfigliuolo@gmail.com>
2025-11-05 17:12:35 -0300

    Add files via upload

---
+++ nigga_code.ino
@@ -0,0 +1,171 @@
+#include <Arduino.h>
+
+// Pines
+const int m1 = 4, m2 = 5, m3 =22 , m4 = 23; // L298N señales
+const int sdl11 = 13, sdl12 = 14, sdl13 = 26, sdl14 = 27;
+const int trigp = 17, echop = 16;
+const int led1 = 18, led2 = 19, led3 = 21;
+const int botoninit = 33;
+
+float dis = 1000;
+unsigned long startFightTime = 0; // para el bien de los ojos del los miembros del grupo, variables las cuales sus nombres si son descriptivas
+bool fightEnabled = false;
+
+// int waitin = 0
+// int countdown = 1
+enum State {
+  WAITING,
+  COUNTDOWN,
+  EXPLOSIVE,
+  SAFE_FIGHT
+}; // lo use para que sea legible
+State robotState = WAITING;
+
+// funciones motores (L298N full power se llama cuando es al mango)
+void stopMotors(){
+  digitalWrite(m1, LOW); digitalWrite(m2, LOW);
+  digitalWrite(m3, LOW); digitalWrite(m4, LOW);
+}
+
+void forward(){
+  digitalWrite(m1, HIGH); digitalWrite(m2, LOW);
+  digitalWrite(m3, HIGH); digitalWrite(m4, LOW);
+}
+
+void backward(){
+  digitalWrite(m1, LOW); digitalWrite(m2, HIGH);
+  digitalWrite(m3, LOW); digitalWrite(m4, HIGH);
+}
+
+void turnRight(){
+  digitalWrite(m1, HIGH); digitalWrite(m2, LOW);
+  digitalWrite(m3, LOW);  digitalWrite(m4, HIGH);
+}
+
+void turnLeft(){
+  digitalWrite(m1, LOW);  digitalWrite(m2, HIGH);
+  digitalWrite(m3, HIGH); digitalWrite(m4, LOW);
+}
+
+// tarea del multi: sensores de linea (ahora en priori max)
+void TaskLineSensor(void *pv){
+  for(;;){
+    bool linea = (digitalRead(sdl11)==LOW || digitalRead(sdl12)==LOW ||
+                  digitalRead(sdl13)==LOW || digitalRead(sdl14)==LOW);
+
+    if(linea && fightEnabled){
+      backward();
+      vTaskDelay(300/portTICK_PERIOD_MS);
+      turnRight();
+      vTaskDelay(300/portTICK_PERIOD_MS);
+    }
+
+    vTaskDelay(1);
+  }
+}
+
+// tarea multisonido
+void TaskUltrasonic(void *pv){
+  for(;;){
+    digitalWrite(trigp, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigp, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigp, LOW);
+
+    unsigned long duration = pulseIn(echop, HIGH, 20000);
+    if(duration > 0) dis = duration * 0.034 / 2;
+    else dis = 200; // nada detectado
+
+    vTaskDelay(30 / portTICK_PERIOD_MS);
+  }
+}
+
+// tarea logica de pelea
+void TaskCombat(void *pv){
+  for(;;){
+    if(!fightEnabled){
+      stopMotors();
+      vTaskDelay(10);
+      continue;
+    }
+
+    //  2.5s inicial
+    if(robotState == EXPLOSIVE){
+      forward();
+      if(millis() - startFightTime >= 2500){
+        robotState = SAFE_FIGHT;
+      }
+    }
+    else if(robotState == SAFE_FIGHT){
+      if(dis <= 30){
+        forward(); // atacar
+      } else {
+        turnRight(); // buscar
+      }
+    }
+
+    vTaskDelay(10);
+  }
+}
+
+// start del coso de inicio oficial sumo
+void TaskStart(void *pv){
+  for(;;){
+    if(digitalRead(botoninit) == HIGH && robotState == WAITING){
+      robotState = COUNTDOWN;
+
+      digitalWrite(led1, HIGH); digitalWrite(led2, LOW); digitalWrite(led3, LOW);
+      vTaskDelay(50 / portTICK_PERIOD_MS); 
+      digitalWrite(led1, LOW); digitalWrite(led2, HIGH);
+      vTaskDelay(50 / portTICK_PERIOD_MS); 
+
+      fightEnabled = true;
+      robotState = EXPLOSIVE;
+      startFightTime = millis();
+    }
+    vTaskDelay(20);
+  }
+}
+
+// tarea leds estado
+void TaskLED(void *pv){
+  for(;;){
+    if(robotState == WAITING) {
+      digitalWrite(led1, HIGH); digitalWrite(led2, LOW); digitalWrite(led3, LOW); // amarillo, verde, rojo
+    } else if(robotState == COUNTDOWN){
+      vTaskDelay(50);
+      digitalWrite(led1, LOW); digitalWrite(led2, HIGH); digitalWrite(led3, LOW);
+    } else {
+      digitalWrite(led1, LOW); digitalWrite(led2, LOW); digitalWrite(led3, HIGH);
+    }
+    vTaskDelay(100);
+  }
+}
+
+// setup
+void setup() {
+  pinMode(m1, OUTPUT); pinMode(m2, OUTPUT);
+  pinMode(m3, OUTPUT); pinMode(m4, OUTPUT);
+
+  pinMode(sdl11, INPUT); pinMode(sdl12, INPUT);
+  pinMode(sdl13, INPUT); pinMode(sdl14, INPUT);
+
+  pinMode(trigp, OUTPUT); pinMode(echop, INPUT);
+  pinMode(led1, OUTPUT); pinMode(led2, OUTPUT); pinMode(led3, OUTPUT);
+  pinMode(botoninit, INPUT);
+
+  Serial.begin(115200);
+
+  xTaskCreatePinnedToCore(TaskLineSensor, "linea", 2000, NULL, 3, NULL, 1);
+  xTaskCreatePinnedToCore(TaskUltrasonic, "ultrasonico", 2000, NULL, 2, NULL, 1);
+  xTaskCreatePinnedToCore(TaskCombat, "combate", 2000, NULL, 2, NULL, 1);
+  xTaskCreatePinnedToCore(TaskStart, "arranque", 2000, NULL, 3, NULL, 0);
+  xTaskCreatePinnedToCore(TaskLED, "leds", 1500, NULL, 1, NULL, 0);
+  // xTaskCreatePinnedToCore(fn, ndebug, size_stack, params para la tarea, prioridad, handle, core)
+  // size = "words"x4bytes=x p/stack
+  stopMotors();
+}
+
+// loop vacio (freertos controla todo entonces no es necesario usar loop)
+void loop(){}
